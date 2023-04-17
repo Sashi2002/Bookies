914f6c975360835d78f9c97b0bef2fd2293c0d34
ha1109@srmist.edu.in <ha1109@srmist.edu.in>
2023-04-17 22:58:54 +0530

    footer added

---
+++ components/footer.js
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import bluneo from "../public/logo.png";
+import { FaDiscord } from "react-icons/fa";
+import { SiTwitter } from "react-icons/si";
+import { AiFillInstagram } from "react-icons/ai";
+import { ImLinkedin } from "react-icons/im";
+import { SiOpensea } from "react-icons/si";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center flex-row max-lg:flex-col-reverse max-lg:gap-4 sm:mx-20 ">
+      <div className="flex justify-center items-center lg:items-start  flex-col lg:w-2/5">
+        <span className="text-6xl max-sm:text-3xl font-bold ">Bookies</span>
+        <p className="sm:text-2xl">
+          "A word after a word after a word is power."
+        </p>
+        <div className="flex justify-center items-center flex-row gap-6 my-6">
+          <span className="h-8 w-8">
+            <FaDiscord className="h-full w-full" />
+          </span>
+          <span className="h-8 w-8">
+            <SiTwitter className="h-full w-full" />
+          </span>
+          <span className="h-8 w-8">
+            <AiFillInstagram className="h-full w-full" />
+          </span>
+          <span className="h-8 w-8">
+            <ImLinkedin className="h-full w-full" />
+          </span>
+          <span className="h-8 w-8">
+            <SiOpensea className="h-full w-full" />
+          </span>
+        </div>
+        <div className="flex justify-center items-center flex-row max-sm:flex-col gap-2 ">
+          <button class="bg-black hover:bg-red-500 text-white border-2 border-black font-bold p-2 rounded-full h-12 w-40 max-sm:w-72">
+            Subscription
+          </button>
+          <button class="bg-white text-black border-2 border-black font-bold p-2 rounded-full h-12 w-40 max-sm:w-72">
+            Community
+          </button>
+        </div>
+        <span className="text-sm mt-6">Agrawal Book Stores </span>
+      </div>
+      <div className="flex justify-center items-center lg:w-3/5">
+        <Image src={bluneo} />
+      </div>
+    </div>
+  );
+};
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Navbar from "../../components/navbar/navbar";
+import Footer from "../../components/footer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <div></div>
+      <Footer />
     </main>
   );
 }
